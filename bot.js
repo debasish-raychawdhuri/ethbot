1d75c2fd8e68711000694170d0b665be02d80c23
Debasish Ray Chawdhuri <debasish.raychawdhuri@gmail.com>
2022-04-01 17:45:59 +0530

    dfd

--- bot.js
+++ bot.js
@@ -50,6 +50,7 @@ async function main(){
 
 		
 	const estimator = new web3.eth.Contract(abi,contractId);
+	allLoop(estimator,miningpk,9)();
 	setInterval(allLoop(estimator,miningpk,9),300000);
 
 	// estimate scale
